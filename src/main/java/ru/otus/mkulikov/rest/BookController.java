df7b8d3208f6ccff1b3256cf5322f555196d336f
Куликов Максим Валерьевич <Kulikov.M.V@sberbank.ru>
2019-08-26 18:19:51 +0300

    Доработка страниц.

--- src/main/java/ru/otus/mkulikov/rest/BookController.java
+++ src/main/java/ru/otus/mkulikov/rest/BookController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.otus.mkulikov.app.model.Author;
 import ru.otus.mkulikov.app.model.Book;
 import ru.otus.mkulikov.app.service.BookManageSevice;
 
@@ -20,5 +22,12 @@ public class BookController {
         List<Book> books = bookManageSevice.getBooks();
         model.addAttribute("books", books);
         return "books";
+    }
+
+    @GetMapping("/book")
+    public String getById(@RequestParam("id") String id, Model model) {
+        Book book = bookManageSevice.getBookById(id);
+        model.addAttribute("book", book);
+        return "book";
     }
 }
