de886cf4d996818b004ea67cb87f7ce957a91ff8
Куликов Максим Валерьевич <Kulikov.M.V@sberbank.ru>
2019-09-18 18:24:08 +0300

    Редактирование книги для комментария. Редактирование жанра.

--- src/main/java/ru/otus/mkulikov/rest/CommentController.java
+++ src/main/java/ru/otus/mkulikov/rest/CommentController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.otus.mkulikov.app.model.Author;
+import ru.otus.mkulikov.app.model.Book;
 import ru.otus.mkulikov.app.model.Comment;
+import ru.otus.mkulikov.app.model.Genre;
+import ru.otus.mkulikov.app.service.BookManageService;
 import ru.otus.mkulikov.app.service.CommentManageService;
 
 import java.util.List;
@@ -16,6 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentController {
 
+    private final BookManageService bookManageSevice;
     private final CommentManageService commentManageService;
 
     @GetMapping("/comment/list")
@@ -36,6 +41,17 @@ public class CommentController {
     public String edit(@ModelAttribute Comment comment, Model model) {
         Comment updated = commentManageService.updateComment(comment);
         model.addAttribute("comment", updated);
+        return "comment";
+    }
+
+    @GetMapping("/comment/new")
+    public String getNew(Model model) {
+        List<Book> books = bookManageSevice.getBooks();
+        Comment comment = commentManageService.addComment("", "", "");
+
+        model.addAttribute("books", books);
+        model.addAttribute("comment", comment);
+
         return "comment";
     }
 }
